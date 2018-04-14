f6c5e224bfbc8019589c795bb0ac22f70d2e6a53
kattisrik <srikanthkatti.katti@gmail.com>
2018-04-14 12:12:34 +0000

    Added extra basic front end

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,5 +1,22 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope) {
-    $scope.array;
+    $scope.message = [
+        {
+            'sender': 'USER',
+            'text': 'Hello'
+},
+        {
+            'sender': 'BOT',
+            'text': 'Hi what can i do for you?'
+},
+        {
+            'sender': 'USER',
+            'text': 'what is the time?'
+},
+        {
+            'sender': 'BOT',
+            'text': '5:30 pm'
+}
+    ]
 });
