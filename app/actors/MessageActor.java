ced1ae9d57610fe0557917766eaefc0d4cd62451
kattisrik <srikanthkatti.katti@gmail.com>
2018-04-15 06:03:14 +0000

    added message actor

--- app/actors/MessageActor.java
+++ app/actors/MessageActor.java
@@ -1,12 +1,60 @@
 package actors;
 
+import akka.actor.ActorRef;
+import akka.actor.Props;
 import akka.actor.UntypedActor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import data.FeedResponse;
+import data.Message;
+import data.NewsAgentResponse;
+import services.FeedService;
+import services.NewsAgentService;
+
+import java.util.UUID;
 
 public class MessageActor extends UntypedActor {
+    //Self - Reference the Actor
+    //  PROPS
+//Object of feed service
+    //object of newsAgentService
+    //Define another actor Reference
+    private final ActorRef out;
+
+    public FeedService feedService = new FeedService();
+    public NewsAgentService newsAgentService = new NewsAgentService();
+
+    public static Props props(ActorRef out) {
+        return Props.create(MessageActor.class, out);
+    }
+
+    public MessageActor(ActorRef out) {
+        this.out = out;
+    }
+
+    public NewsAgentResponse newsAgentResponse = new NewsAgentResponse();
+    public FeedResponse feedResponse = new FeedResponse();
 
 
     @Override
     public void onReceive(Object message) throws Throwable {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        if (message instanceof String) {
+            Message messageObject = new Message();
+            messageObject.text = message.toString();
+            messageObject.sender = Message.Sender.USER;
+            out.tell(objectMapper.writeValueAsString(messageObject), self());  //to send message over ws
+            newsAgentResponse = newsAgentService.getNewsAgentResponse(messageObject.text, UUID.randomUUID());
+            feedResponse = feedService.getFeedByQuery(newsAgentResponse.query);
+            messageObject.text = (feedResponse.title == null) ? "No results found" : "Showing results for: " + newsAgentResponse.query;
+            messageObject.feedResponse = feedResponse;
+            messageObject.sender = Message.Sender.BOT;
+            out.tell(objectMapper.writeValueAsString(messageObject), self());
+
+
+        }
+
 
     }
 }
--- app/data/Message.java
+++ app/data/Message.java
@@ -1,5 +1,9 @@
 package data;
 
 public class Message {
+public enum Sender {USER,BOT};
+    public String text;
+    public FeedResponse feedResponse = new FeedResponse();
+    public Sender sender;
 
 }
