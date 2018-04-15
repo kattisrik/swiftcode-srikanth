be057467c50f199711d4664084194be77ee9fd56
kattisrik <srikanthkatti.katti@gmail.com>
2018-04-15 12:18:13 +0000

    added final +

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -12,7 +12,7 @@ app.controller('chatController', function ($scope, $sce) {
 
     $scope.message = [];
     $scope.trust = $sce.trustAsHtml;
-    var exampleSocket = new WebSocket("ws://localhost:9000/chatSocket");
+    var exampleSocket = new WebSocket("wss://swiftcode-newsbot-srikanth.herokuapp.com/chatSocket");
 
     exampleSocket.onmessage = function (event) {
         var jsonData = JSON.parse(event.data);
